53cdfff676c77eb31fb5adc32dead7aeaf5aa905
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-05-12 13:32:19 -0400

    debugging and reworking

--- MD5Hash.pl
+++ MD5Hash.pl
@@ -2,7 +2,7 @@
 # Earl Platt III
 # 5/5/2019
 # BCS 316 - Perl Programming
-# MD5 Hash
+# MD5 Hash Part I
 # Input - User Enters Username & Password
 # Output - Gives Access or not
 ##########################################
@@ -13,7 +13,7 @@ use Digest::MD5 qw(md5 md5_hex md5_base64);
 
 my $username;
 my $password;
-my $loginFile = 'loginfile.txt'
+my $loginFile = 'loginfile.txt';
 
 print "\n";
 print "Enter a Username & Password";
@@ -21,16 +21,19 @@ print "\n";
 
 print "Username: ";
 $username = <STDIN>;
+chomp $username;
 print "\n";
 
 print "Password: ";
 $password = <STDIN>;
+chomp $password;
 
 my $md5Password = md5($password);
 
 open(my $file, '>', $loginFile) or die;
 
 print $file "$username";
+print $file "\n";
 print $file "$md5Password";
 
 close $file;
